2296b06fabc1a4683354f7e54f0c52419c0e9fbc
Sean Ryan <sean.ryan@mendix.com>
2019-01-07 15:19:30 +0100

    fix: Modify install.sh so curl can download fully

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded #
 sudo apt-get update
 
 sudo apt install git
@@ -7,3 +10,4 @@ git clone https://github.com/mrseanryan/ubuntu-scripts.git
 cd ubuntu-scripts
 
 ./go.sh
+} # this ensures the entire script is downloaded #
