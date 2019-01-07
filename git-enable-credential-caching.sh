262019fe73dd24a1390bc388d655aa7131b3519d
Sean <str_ie@yahoo.co.uk>
2019-01-07 11:18:05 +0100

    fix: Fix the git credential timeout to 3 months

--- git-enable-credential-caching.sh
+++ git-enable-credential-caching.sh
@@ -1,2 +1,5 @@
 git config --global credential.helper cache
-git config --global crendtial.helper 'cache --timeout=3000000'
+# 1 month == 2.6 million seconds
+# 3 monthds == 7.8 million seconds
+git config --global credential.helper 'cache --timeout=7800000'
+
