98b8c88f226fa271bfa152fc8740fdb7c78f7731
sean <str_ie@yahoo.co.uk>
2019-01-09 10:15:14 +0100

    chore: Tweak code alias so does not block updated version

--- install-visual-code.sh
+++ install-visual-code.sh
@@ -5,9 +5,11 @@ sudo apt-get --yes --allow-downgrades install ubuntu-make
 
 umake ide visual-studio-code
 
-echo alias code=~/.local/share/umake/ide/visual-studio-code/bin/code >> ~/.bashrc
+echo alias visual-code=~/.local/share/umake/ide/visual-studio-code/bin/code >> ~/.bashrc
 
 source ~/.bashrc
 
-echo Type 'code' to launch Visual Code
+echo Type 'visual-code' to launch Visual Code
+
+echo note: updating Visual Code will install to /usr/bin/code
 
