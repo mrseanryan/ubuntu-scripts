15d1aac207f2d56e6e7aa4cd2b2fdb481041aace
Sean Ryan <sean.ryan@mendix.com>
2019-04-29 10:02:53 +0200

    Up version of docker-compose

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -3,7 +3,7 @@
 
 TMP=`mktemp -d 2>/dev/null || mkdtemp -d -t 'temp'`
 
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` > $TMP/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` > $TMP/docker-compose
 chmod +x $TMP/docker-compose
 
 sudo mv $TMP/docker-compose /usr/bin/
