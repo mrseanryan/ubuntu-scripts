5c6db406fa2127ddf26329ea307ae6769528392f
sean <str_ie@yahoo.co.uk>
2019-09-19 21:02:03 +0200

    Enhance clear script to clear old snaps

--- useful-scripts/clear-unused-ubuntu-packages.sh
+++ useful-scripts/clear-unused-ubuntu-packages.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
 # ref: https://www.fosslinux.com/2986/free-up-hard-disk-space-ubuntu.htm
 sudo apt-get -s clean
 sudo apt-get autoremove --purge
+
+# Removes old revisions of snaps
+# CLOSE ALL SNAPS BEFORE RUNNING THIS
+set -eu
+snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
