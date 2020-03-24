ed8c9a2c9fa09eb70ba9164870c3bec897b0284e
Sean Ryan <str_ie@yahoo.co.uk>
2020-03-24 21:14:18 +0100

    Fix the not secure ftp script and the secure ftp script.

--- extra-install-ftp-server-NOT-secure.sh
+++ extra-install-ftp-server-NOT-secure.sh
@@ -7,8 +7,8 @@ if [ ! -f /etc/vsftpd.conf.bak ]; then
   sudo mv /etc/vsftpd.conf /etc/vsftpd.conf.bak
 fi
 
-sudo cp ./config/ftp/vsftpd.conf /etc/vsftpd.conf
-sudo cp ./config/ftp/vsftpd.chroot_list /etc/vsftpd.chroot_list
+sudo cp ./config/ftp/vsftpd.conf.NOT-secure /etc/vsftpd.conf
+sudo cp ./config/ftp/vsftpd.chroot_list.NOT-secure /etc/vsftpd.chroot_list
 
 sudo systemctl restart vsftpd
 
