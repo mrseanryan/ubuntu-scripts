fdaca074133c0a89cdc23e67348b1246d1a67334
Sean Ryan <str_ie@yahoo.co.uk>
2023-08-09 18:44:30 +0200

    Up git config for git 2.34.1. Fixes #1

--- extra-install-git-rerere-etc.sh
+++ extra-install-git-rerere-etc.sh
@@ -8,7 +8,10 @@ echo "  To smoothen the experience, enable rerere 'reuse recorded resolution' in
 git config --global rerere.enabled true
 
 echo " - For the same reasons, use rebase-on-pull instead of merge-on-pull"
-git config --global pull.rebase preserve
+# older git versions:
+#git config --global pull.rebase preserve
+# newer git versions: (2.34.1) [Ubuntu 22.04]
+git config --global pull.rebase merges
 
 echo " - disable fast-forward merges"
 git config --global merge.ff false
